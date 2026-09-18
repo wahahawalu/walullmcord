66d5b1d06b8e4936b96ae7f53e636f7a46f6918c
walu <waruiji@proton.me>
2026-09-18 22:42:51 +1000

    Fingers crossed
    I have no idea what I'm doing

--- llmcord.py
+++ llmcord.py
@@ -52,7 +52,7 @@ last_task_time = 0
 
 intents = discord.Intents.default()
 intents.message_content = True
-activity = discord.CustomActivity(name=(config.get("status_message") or "github.com/jakobdylanc/llmcord")[:128])
+activity = discord.CustomActivity(name=(config.get("status_message") or "WAAAAAH!")[:128])
 discord_bot = commands.Bot(intents=intents, activity=activity, command_prefix=None)
 
 httpx_client = httpx.AsyncClient()
